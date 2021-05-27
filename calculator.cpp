314df603f385d2982eb8cab7eed324e6b5ccfca8
rahulsinghbisht-13 <rbisht733@gmail.com>
2021-05-27 23:52:37 +0530

    Added a calculator c++ program, First commit by Master branch

---
+++ calculator.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	char ch;
+	do {
+		
+		int num1, num2;
+		cout << "Enter Two Numbers: ";
+		cin >> num1 >> num2;
+		cout << endl;
+		
+		cout <<"Enter an operator--> (+, -, *, /) or (X) to quit : ";
+		cin >> ch;
+		cout << endl;
+		
+		int res = 0;
+	
+		switch(ch)
+		{
+			case '+':
+				res = num1 + num2;
+				cout << "Result = " << res << endl;
+				break;
+				
+			case '-':
+				res = num1 - num2;
+				cout << "Result = " << res << endl;
+				break;
+				
+			case '*':
+				res = num1 * num2;
+				cout << "Result = " << res << endl;
+				break;
+				
+			case '/':
+				res = num1 / num2;
+				cout << "Result = " << res << endl;
+				break;
+				
+			case 'X':
+				break;
+				
+			default :
+				cout << "INVALID CHOICE!!! PLEASE ENTER CORRECT OPERATOR...";
+				break;
+		}
+		cout << "\n----------------------------------------------------\n";
+	}while(ch != 'X');
+	
+	
+	return 0;
+}
